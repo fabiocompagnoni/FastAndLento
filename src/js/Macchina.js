1ba4dd9825c616ce66fb757513add039d6f9f5f2
fabiocompagnoni <fabiocompagnoni@smanettone.eu>
2021-03-05 21:43:44 +0100

    avanti con il lavoro

--- src/js/Macchina.js
+++ src/js/Macchina.js
@@ -14,7 +14,6 @@ class Macchina
     {
         this.modello=modello;
         this.vel=0;
-        posX=Math.random()
     }
     getPosX()
     {
@@ -27,5 +26,41 @@ class Macchina
     setPosX(posX)
     {
         this.posX=posX;
+    }
+    setPosY(posY)
+    {
+        this.posY=posY;
+    }
+    getModello()
+    {
+        return this.modello;
+    }
+    setModello(modello)
+    {
+        this.modello=modello;
+    }
+    muovi(X,Y)
+    {
+        this.posX+=X;
+        this.posY+=Y;
+    }
+    mostra(canvas)
+    {
+        let str="";
+        switch(this.modello)
+        {
+            case 0: str="Audi"; break;
+            case 1: str="Car"; break;
+            case 2: str="Mini_truck"; break;
+            case 3: str="Mini_van"; break;
+            case 4: str="Police"; break;
+            case 5: str="Ambulance"; break;
+        }
+        str+=".png";
+        let ctx=canvas.getContext("2d");
+        let img=document.createElement("img");
+        img.src='src/img/macchine/'+str;
+        img.appendChild(canvas);
+        ctx.drawImage(img,this.posX,this.posY);
     }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -6,5 +6,46 @@ class Giocatore
 {
     static punteggio;
     static modelloMacchina;
-    
+    constructor(punteggio,modelloMacchina)
+    {
+        this.punteggio=punteggio;
+        this.modelloMacchina=modelloMacchina;
+    }
+    getPunteggio()
+    {
+        return this.punteggio
+    }
+    setPunteggio(punteggio)
+    {
+        this.punteggio=punteggio;
+    }
+    updatePunteggio(toAdd)
+    {
+        this.punteggio+=toAdd;
+    }
+    getModelloMacchina()
+    {
+        return this.modelloMacchina;
+    }
+    setModelloMacchina(modello)
+    {
+        this.modelloMacchina=modello;
+    }
 }
+
+//giocatore principale
+const giocatore1=new Giocatore(0,0);
+//macchinaDelGiocatore
+const macchinaPrincipale=new Macchina(giocatore1.getModelloMacchina());
+
+const canvas=document.getElementById("gioco");
+
+//genero 5 macchine
+const macchine=new Array();
+for(let i=0;i<5;i++)
+{
+    macchine[i]=new Macchina((Math.floor(Math.random()*5)));
+    macchine[i].mostra(canvas);
+}
+
+
